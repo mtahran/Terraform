340aefc366249ff752865a095901ca086370cec3
mtahran <mutahran@gmail.com>
2023-06-17 22:16:21 +0000

    new repository for Terraform

---
+++ resource-data-source/for_each-example/variables.tf
@@ -0,0 +1,36 @@
+variable "sg_ingress_rules" {
+  type = map(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+  default = {
+   1 = {
+     type        = "ingress"
+     protocol    = "tcp"
+     from_port   = 22
+     to_port     = 22
+     cidr_block  = "98.227.136.153/32"
+     description = "test"
+   },
+   2 = {
+     type        = "ingress"
+     protocol    = "tcp"
+     from_port   = 80
+     to_port     = 80
+     cidr_block  = "0.0.0.0/0"
+     description = "test"
+   }
+  }
+}
+
+variable "aws_secret_key" {
+  default = ""
+}
+
+variable "aws_access_key" {
+  default = ""
+}
